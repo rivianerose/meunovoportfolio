5402e99958ddf69e2ac4087dd03c951bdae6dbb5
Riviane Rose <107583014+rivianerose@users.noreply.github.com>
2023-08-28 18:37:06 -0300

    add

--- pages/backend/app.js
+++ pages/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { db, insertFormData } = require('./database');
+// const nodemailer = require('nodemailer');
 const app = express();
 const port = 4000;
 
@@ -11,8 +12,43 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+
+// // Função para enviar e-mail de resposta
+// function sendEmail(name, email) {
+//   const transporter = nodemailer.createTransport({
+//     service: 'Outlook',
+//     auth: {
+//       user: 'babydeeh1@gmail.com',
+//       pass: 'GATONA98211065'
+//     }
+//   });
+
+//   const mailOptions = {
+//     from: 'babydeeh1@gmail.com',
+//     to: email,
+//     subject: 'Obrigado por enviar o formulário',
+//     text: `Olá ${name},\n\nObrigado por enviar o formulário. Recebemos seus dados com sucesso.\n\nAtenciosamente,\nSua Equipe`
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.error('Erro ao enviar o e-mail:', error);
+//     } else {
+//       console.log('E-mail enviado:', info.response);
+//     }
+//   });
+// }
+
+
 app.post('/submit', (req, res) => {
   const formData = req.body;
+
+
+  const dataWithMesage = {
+    name: formData.name,
+    email: formData.email,
+    field3: formData.message // Certifique-se de que o campo3 no objeto corresponda ao nome no formulário HTML
+  };
   insertFormData(formData, (err) => {
     if (err) {
       console.error('Erro ao inserir os dados:', err);
@@ -20,6 +56,11 @@ app.post('/submit', (req, res) => {
     } else {
       console.log('Dados inseridos com sucesso.');
       res.send('Dados do formulário salvos com sucesso no banco de dados.');
+
+       // Enviar e-mail de resposta
+      //  sendEmail(formData.name, formData.email);
+
+      //  res.send('Dados do formulário salvos com sucesso no banco de dados.');
     }
   });
 });
--- pages/backend/database.js
+++ pages/backend/database.js
@@ -1,21 +1,22 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Abre ou cria o banco de dados
-const db = new sqlite3.Database('formulariosdois.db');
+const db = new sqlite3.Database('formulariostres.db');
 
 // Cria a tabela se ela não existir
 db.run(`
-  CREATE TABLE IF NOT EXISTS formulario (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    campo1 TEXT,
-    campo2 TEXT
-  )
+CREATE TABLE IF NOT EXISTS formulario (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  campo1 TEXT,
+  campo2 TEXT,
+  campo3 TEXT
+)
 `);
 
 // Função para inserir dados
 function insertFormData(data, callback) {
-  const sql = 'INSERT INTO formulario (campo1, campo2) VALUES (?, ?)';
-  db.run(sql, [data.name, data.email], callback);
+  const sql = 'INSERT INTO formulario (campo1, campo2, campo3) VALUES (?, ?, ?)';
+  db.run(sql, [data.name, data.email, data.message], callback);
 }
 
 module.exports = { db, insertFormData };
